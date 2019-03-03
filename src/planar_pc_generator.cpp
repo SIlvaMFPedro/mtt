0c7b8cd93b32df6294b28f7096facb3fb5931028
Pedro Silva <silva.mfpedro@gmail.com>
2019-03-03 15:58:49 +0000

    Added planar_pc_generator source file to mtt project folder

--- src/planar_pc_generator.cpp
+++ src/planar_pc_generator.cpp
@@ -67,109 +67,107 @@ double output_freq;
 
 void filter_cloud(pcl::PointCloud<pcl::PointXYZ>* pc_in, std::string pc_frame_id)
 {
-pcl::PointCloud<pcl::PointXYZ> pc_transformed;
-pcl::PointCloud<pcl::PointXYZ> pc_filtered;
-pcl::PointCloud<pcl::PointXYZ> pc_projected;
-tf::StampedTransform transform;
+    pcl::PointCloud<pcl::PointXYZ> pc_transformed;
+    pcl::PointCloud<pcl::PointXYZ> pc_filtered;
+    pcl::PointCloud<pcl::PointXYZ> pc_projected;
+    tf::StampedTransform transform;
 
-//p_listener->waitForTransform(pcmsg_in.header.frame_id, ros::names::remap("/tracking_frame"),ros::Time::now(), ros::Duration(0.2));
-try
-{
-p_listener->lookupTransform(pc_frame_id, ros::names::remap("/tracking_frame"), ros::Time(0), transform);
-//p_listener->lookupTransform(pcmsg_in.header.frame_id, ros::names::remap("/tracking_frame"), scan_in->header.stamp, transform);
-}
-catch (tf::TransformException ex)
-{
-ROS_ERROR("%s",ex.what());
-}
+    //p_listener->waitForTransform(pcmsg_in.header.frame_id, ros::names::remap("/tracking_frame"),ros::Time::now(), ros::Duration(0.2));
+    try
+    {
+        p_listener->lookupTransform(pc_frame_id, ros::names::remap("/tracking_frame"), ros::Time(0), transform);
+        //p_listener->lookupTransform(pcmsg_in.header.frame_id, ros::names::remap("/tracking_frame"), scan_in->header.stamp, transform);
+    }
+    catch (tf::TransformException ex)
+    {
+        ROS_ERROR("%s",ex.what());
+    }
 
+    pcl_ros::transformPointCloud(*pc_in,pc_transformed,transform.inverse());
+    pc_transformed.header.frame_id = ros::names::remap("/tracking_frame");
+    //pc_transformed.header.stamp = scan_in->header.stamp;
 
-pcl_ros::transformPointCloud(*pc_in,pc_transformed,transform.inverse());
-pc_transformed.header.frame_id = ros::names::remap("/tracking_frame");
-//pc_transformed.header.stamp = scan_in->header.stamp;
+    pcl::ExtractPolygonalPrismData<pcl::PointXYZ> epp;
+    pcl::PointCloud<pcl::PointXYZ>::ConstPtr input_cloud_constptr;
+    input_cloud_constptr.reset (new pcl::PointCloud<pcl::PointXYZ> (pc_transformed));
+    pcl::PointCloud<pcl::PointXYZ>::ConstPtr convex_hull_constptr;
+    convex_hull_constptr.reset (new pcl::PointCloud<pcl::PointXYZ> (convex_hull));
 
-pcl::ExtractPolygonalPrismData<pcl::PointXYZ> epp;
-pcl::PointCloud<pcl::PointXYZ>::ConstPtr input_cloud_constptr;
-input_cloud_constptr.reset (new pcl::PointCloud<pcl::PointXYZ> (pc_transformed));
-pcl::PointCloud<pcl::PointXYZ>::ConstPtr convex_hull_constptr;
-convex_hull_constptr.reset (new pcl::PointCloud<pcl::PointXYZ> (convex_hull));
+    //pcl::PointIndices::Ptr ind =  pcl::PointIndices::Ptr(new pcl::PointIndices);
+    pcl::ExtractIndices<pcl::PointXYZ> extract; //Create the extraction object
+    pcl::PointIndices::Ptr indices;
+    indices.reset();
+    indices = pcl::PointIndices::Ptr(new pcl::PointIndices);
 
-//pcl::PointIndices::Ptr ind =  pcl::PointIndices::Ptr(new pcl::PointIndices);
-pcl::ExtractIndices<pcl::PointXYZ> extract; //Create the extraction object
-pcl::PointIndices::Ptr indices;
-indices.reset();
-indices = pcl::PointIndices::Ptr(new pcl::PointIndices);
+    //Set epp parameters
+    epp.setInputCloud(input_cloud_constptr);
+    epp.setInputPlanarHull(convex_hull_constptr);
+    epp.setHeightLimits(-perpendicular_treshold, perpendicular_treshold);
+    epp.setViewPoint(0,0,0); //i dont think this serves any purpose in the case of epp
+    epp.segment(*indices);
 
-//Set epp parameters
-epp.setInputCloud(input_cloud_constptr);
-epp.setInputPlanarHull(convex_hull_constptr);
-epp.setHeightLimits(-perpendicular_treshold, perpendicular_treshold);
-epp.setViewPoint(0,0,0); //i dont think this serves any purpose in the case of epp
-epp.segment(*indices);
+    extract.setInputCloud(pc_transformed.makeShared());
+    extract.setIndices(indices);
+    extract.setNegative(false);
+    extract.filter(pc_filtered);
 
-extract.setInputCloud(pc_transformed.makeShared());
-extract.setIndices(indices);
-extract.setNegative(false);
-extract.filter(pc_filtered);
+    pcl::ProjectInliers<pcl::PointXYZ> projection;
+    projection.setModelType(pcl::SACMODEL_PLANE); //set model type
+    pcl::ModelCoefficients::Ptr coeff(new pcl::ModelCoefficients);
+    coeff->values.resize(4);
+    coeff->values[0] = 0;
+    coeff->values[1] = 0;
+    coeff->values[2] = 1;
+    coeff->values[3] = 0;
 
-pcl::ProjectInliers<pcl::PointXYZ> projection;
-projection.setModelType(pcl::SACMODEL_PLANE); //set model type
-pcl::ModelCoefficients::Ptr coeff(new pcl::ModelCoefficients);
-coeff->values.resize(4);
-coeff->values[0] = 0;
-coeff->values[1] = 0;
-coeff->values[2] = 1;
-coeff->values[3] = 0;
+    projection.setInputCloud(pc_filtered.makeShared());
+    projection.setModelCoefficients(coeff);
+    projection.filter(pc_projected);
 
-projection.setInputCloud(pc_filtered.makeShared());
-projection.setModelCoefficients(coeff);
-projection.filter(pc_projected);
+    coeff.reset();
+    input_cloud_constptr.reset();
+    convex_hull_constptr.reset();
+    indices.reset();
 
-coeff.reset();
-input_cloud_constptr.reset();
-convex_hull_constptr.reset();
-indices.reset();
+    pc_accumulated += pc_projected;
 
-pc_accumulated += pc_projected;
-
-#if _USE_DEBUG_
-ROS_INFO("pc_transformed has %d points", (int)pc_transformed.size());
-	ROS_INFO("pc_filtered has %d points", (int)pc_filtered.size());
-	ROS_INFO("pc_accumulated now has %d points", (int)pc_accumulated.size());
-#endif
+    #if _USE_DEBUG_
+    ROS_INFO("pc_transformed has %d points", (int)pc_transformed.size());
+        ROS_INFO("pc_filtered has %d points", (int)pc_filtered.size());
+        ROS_INFO("pc_accumulated now has %d points", (int)pc_accumulated.size());
+    #endif
 }
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr & pcmsg_in)
 {
 
-#if _USE_DEBUG_
-    ROS_INFO("Received point cloud");
-#endif
-    pcl::PointCloud<pcl::PointXYZ> pc_in;
-    pcl::PCLPointCloud2 pcl_pc;
-    pcl_conversions::toPCL(*pcmsg_in, pcl_pc);
+    #if _USE_DEBUG_
+        ROS_INFO("Received point cloud");
+    #endif
+        pcl::PointCloud<pcl::PointXYZ> pc_in;
+        pcl::PCLPointCloud2 pcl_pc;
+        pcl_conversions::toPCL(*pcmsg_in, pcl_pc);
 
-    pcl::fromROSMsg(*pcmsg_in,pc_in);
-    filter_cloud(&pc_in, pcmsg_in->header.frame_id);
+        pcl::fromROSMsg(*pcmsg_in,pc_in);
+        filter_cloud(&pc_in, pcmsg_in->header.frame_id);
 }
 
 void scan_cb (const sensor_msgs::LaserScan::ConstPtr& scan_in)
 {
-#if _USE_DEBUG_
-    ROS_INFO("Received laser scan");
-#endif
+    #if _USE_DEBUG_
+        ROS_INFO("Received laser scan");
+    #endif
+        ROS_INFO("asdasd");
+        laser_geometry::LaserProjection projector;
+        sensor_msgs::PointCloud2 pcmsg_in;
 
-    ROS_INFO("asdasd");
-    laser_geometry::LaserProjection projector;
-    sensor_msgs::PointCloud2 pcmsg_in;
+        projector.transformLaserScanToPointCloud(scan_in->header.frame_id, *scan_in,pcmsg_in,*p_listener);   // não é necessário
+        pcmsg_in.header.stamp=ros::Time();
+        pcmsg_in.header.frame_id=scan_in->header.frame_id;
 
-    projector.transformLaserScanToPointCloud(scan_in->header.frame_id, *scan_in,pcmsg_in,*p_listener);   // não é necessário
-    pcmsg_in.header.stamp=ros::Time();
-    pcmsg_in.header.frame_id=scan_in->header.frame_id;
-
-    pcl::PointCloud<pcl::PointXYZ> pc_in;
-    pcl::fromROSMsg(pcmsg_in,pc_in);
-    filter_cloud(&pc_in, scan_in->header.frame_id);
+        pcl::PointCloud<pcl::PointXYZ> pc_in;
+        pcl::fromROSMsg(pcmsg_in,pc_in);
+        filter_cloud(&pc_in, scan_in->header.frame_id);
 }
 
 
